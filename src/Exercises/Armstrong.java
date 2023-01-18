44a7cfcbf96e97db4d2d48526b259348427477c5
alex <alex>
2023-01-18 15:08:45 +0200

    updateArmstrong

---
+++ src/Exercises/Armstrong.java
@@ -0,0 +1,46 @@
+package Exercises;
+
+import static java.lang.Math.pow;
+
+public class Armstrong {
+
+
+
+    public int riseOfPowef(long number) {
+        int riseOfPower = 0;
+        while(number>0) {
+            long rest = number % 10;
+            number = number/10;
+            riseOfPower++;
+        }
+        return riseOfPower;
+    }
+
+    /*
+     * Check no is Armstrong or not in java Program
+     * */
+    public void armstrongNumber(long number) {
+        long  temp = number;
+        long[] elements = new long[10];
+        long rest;
+        long sum = 0;
+        for (int i = 0; temp > 0; i++) {
+            rest = temp % 10;
+            elements[i] = rest;
+            temp = temp / 10;
+        }
+
+        for (long element : elements) {
+            sum = (int) (sum + pow(element, riseOfPowef(number)));
+        }
+
+        if (number == sum) {
+            System.out.println("este numar Armstrong: " + number);
+
+        }
+//        else {
+//            System.out.println("nu este numar Armstrong");
+//        }
+
+    }
+}
--- src/Exercises/Exercises1.java
+++ src/Exercises/Exercises1.java
@@ -10,94 +10,106 @@ public class Exercises1 {
 
     public static void main(String[] args) {
         Exercises1 test = new Exercises1();
+        Armstrong  numbers = new Armstrong();
         Scanner scanner = new Scanner(System.in);
 
-        test.reverseDigits(scanner.nextInt());
-        test.palindrom(scanner.next());
-        test.largestNumber(230,13,11);
-        test.factorial(10);
-        for(int i = 0;i<1000;i++) {
-            test.armstrongNumber(i);
+//        test.reverseDigits(scanner.nextInt());
+//        test.palindrom(scanner.next());
+//        test.largestNumber(230,13,11);
+//        test.factorial(10);
+        for(long i = 0;i<1000000000000L;i++) {
+            numbers.armstrongNumber(i);
         }
 
-
+//        test.primNumber(test.elements(100));
     }
 
 
-
+    /*
+     *Print Reverse number in java program
+     * */
     public void reverseDigits(int a) {
         int reverse = 0;
-        while(a >0) {
-            reverse = reverse * 10 + a%10;
-            a = a/10;
+        while (a > 0) {
+            reverse = reverse * 10 + a % 10;
+            a = a / 10;
         }
         System.out.println(reverse);
     }
 
-
+    /*
+     * Check Given No is palindrome or Not in java Program
+     * */
     public void palindrom(String val) {
         char[] elemente = val.toCharArray();
         String[] newVal = new String[elemente.length];
 
-        for(int i = elemente.length-1; i>=0; i--) {
+        for (int i = elemente.length - 1; i >= 0; i--) {
             newVal[i] = newVal[i] + elemente[i];
         }
 
-        if(val.equals(Arrays.toString(newVal))) {
+        if (val.equals(Arrays.toString(newVal))) {
             System.out.println("este palindrom " + val + "----" + Arrays.toString(newVal));
-        }else{
+        } else {
             System.out.println("nu este palindrom");
         }
     }
 
-
-    public void largestNumber( int x, int y, int z) {
-        if(x != y && y!= z && x != z) {
-            if(x>y) {
-                if(x>z) {
+    /*
+     * Find Largest no in java Program
+     * */
+    public void largestNumber(int x, int y, int z) {
+        if (x != y && y != z && x != z) {
+            if (x > y) {
+                if (x > z) {
                     System.out.println("val lui x este cea mai mare");
-                }else {
+                } else {
                     System.out.println("val lui z este cea mai mare");
                 }
-            }else if(y>z) {
+            } else if (y > z) {
                 System.out.println("y este cel mai mare");
-            }else {
+            } else {
                 System.out.println("z este cel mai mare");
             }
-        }else {
+        } else {
             System.out.println("numerele adaugate  nu sunt distincte ");
         }
     }
 
-
+    /*
+     * Find factorial for given no Program in Java
+     * */
     public void factorial(int number) {
         int factorial = 1;
-        for(int i = number; i>=1;i--) {
+        for (int i = number; i >= 1; i--) {
             factorial = factorial * i;
         }
-        System.out.println(""+number+" factorial = " + factorial);
+        System.out.println("" + number + " factorial = " + factorial);
     }
 
-    public void armstrongNumber(int number) {
-        int temp = number;
-        int[] elements = new int[10];
-        int rest;
-        int sum = 0;
-
-        for(int i = 0; temp >0;i++) {
-            rest = temp%10;
-            elements[i] = rest;
-            temp = temp/10;
+    /*
+     * Print prime no Program in java
+     * */
+    public int[] elements(int maxim) {
+        int[] numbers = new int[maxim + 1];
+        for (int i = 0; i <= maxim; i++) {
+            numbers[i] = i;
         }
-
-        for (int element : elements) {
-            sum = (int) (sum + pow(element, 3));
-        }
-
-        if(number == sum) {
-            System.out.println("este numar Armstrong: " + number);
-        }else {
-            System.out.println("nu este numar Armstrong");
-        }
+        return numbers;
     }
+
+
+//    public void primNumber(int[] numbers) {
+//        for (int j = 0; j < numbers.length; j++) {
+//            if(x==x) {
+//                System.out.println("numerele prime sunt: " + numbers[j]);
+//            }
+//        }
+//    }
+//
+//        }
+//    }
 }
+
+
+
